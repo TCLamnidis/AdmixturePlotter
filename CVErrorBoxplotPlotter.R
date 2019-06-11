b85761445004ffdbc2def712e82d1328c4194b57
TCLamnidis <thisseass@gmail.com>
2019-06-11 14:14:35 +0200

    CVErrorBoxplotPlotter.R now infers X-axis labels from input data.

--- CVErrorBoxplotPlotter.R
+++ CVErrorBoxplotPlotter.R
@@ -1,4 +1,6 @@
 #!/usr/bin/env Rscript
+library(stringr)
+
 args <- commandArgs(TRUE)
 if (is.na(args[1]) == T || is.na(args[2]) == T ){
   print("Usage: Rscript CVErrorBoxplotPlotter.R input output")
@@ -9,7 +11,8 @@ output <- paste0(sub(x=args[2], replacement="", pattern=".png"),".png") ## Will 
 
 ## CV error distribution for the top K replicates
 CVs = read.table(input, header=T)
-
+minK <- str_remove(names(CVs), "X") [1]
+maxK <- rev(str_remove(names(CVs), "X")) [1]
 ## box plot for CV errors of all replicates
 png(output, height=20, width=1.5*ncol(CVs), res=300, units="cm")
 par(cex.main=1.2, cex.axis=1, cex.lab=1)
@@ -17,5 +20,5 @@ par(mar=c(5.1,4.6,4.1,2.1))
 boxplot(CVs, xlab="K", ylab="CV error", xaxt="n", main="")
 # mtext(3, text=input, line=2.2, cex=1.3, font=2)
 mtext(3, text="CV error for all replicates", line=1.5, cex=1.3, font=2)
-axis(side=1, at=1:ncol(CVs), label=(1:ncol(CVs))+1)
+axis(side=1, at=1:ncol(CVs), label=minK:maxK)
 dev.off()
